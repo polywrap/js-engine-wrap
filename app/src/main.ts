4055cf19a9057b60af93057dfb72808890a6ae3f
Jure Bogunovic <bogunovij@gmail.com>
2023-08-29 20:16:31 +0200

    waiting for jobs

--- app/src/main.ts
+++ app/src/main.ts
@@ -20,8 +20,21 @@ import { PluginPackage } from "@polywrap/plugin-js";
     method: "eval",
     args: {
       src: `
-      const doStuff = () => {
+      const doStuff4 = () => {
         return __wrap_subinvoke("test/a", "meth", { message: "a" });
+      };
+
+      const doStuff3 = () => {
+        return doStuff4();
+      };
+      const doStuff2 = () => {
+        return doStuff3();
+      };
+      const doStuff1 = () => {
+        return doStuff2();
+      };
+      const doStuff = () => {
+        return doStuff1();
       };
       const alert = () => {
         return  doStuff();
